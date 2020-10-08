e85abdb8c05f828840091e0cc56ef79ff8b8b7c9
DaBoop <arti_2002_2011@mail.ru>
2020-10-08 21:32:40 +0300

    Class List, Class Node, Add, Indexer, Constructor

--- Program.cs
+++ Program.cs
@@ -1,28 +1,67 @@
 ﻿using System;
+using System.Runtime.Versioning;
 
 namespace Lab4
 {
     class Program
     {
+        public class Node<T>
+        {
+            public T Item { get; set; }
+
+            public Node<T> Next { get; set; }
+
+            
+            public Node(T item)
+            {
+                Item = item;
+            }
+        }
+
+
         class List<T>
         {
             int itemCount;
-            public T[] item 
-            {
-                get => item;
-                set => item = value;
-            }
+            private Node<T> head;
 
-            public T this[int i]
-            {
-                get => item[i];
-                set => item[i] = value;
-            }
+            Node<T> Head { get => head; set => head = value; }
 
             public List()
             {
                 itemCount = 0;
             }
+
+            public void Add(T val)
+            {
+                Node<T> current = Head;
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+            }
+            public T this[int i]
+            {
+                get
+                {
+                    Node<T> current = Head;
+                    for (int j = 0; i < j; j++)
+                    {
+                        current = current.Next;
+                    }
+                    return current.Item;
+                }
+                set
+                {
+                    Node<T> current = Head;
+                    for (int j = 0; i < j; j++)
+                    {
+                        current = current.Next;
+                    }
+                    current.Item = value;
+                }
+
+            }
+
 
         }
         static void Main(string[] args)
