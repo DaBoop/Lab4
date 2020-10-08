fc7cbddad69b6fa9d0dbdcc274825426b877e065
DaBoop <arti_2002_2011@mail.ru>
2020-10-08 20:50:54 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab4
+{
+    class Program
+    {
+        class List<T>
+        {
+            int itemCount;
+            public T[] item 
+            {
+                get => item;
+                set => item = value;
+            }
+
+            public T this[int i]
+            {
+                get => item[i];
+                set => item[i] = value;
+            }
+
+            public List()
+            {
+                itemCount = 0;
+            }
+
+        }
+        static void Main(string[] args)
+        {
+            
+
+
+
+
+
+            Console.Write("\nPress any key to continue . . . ");
+            Console.ReadKey(true);
+        }
+    }
+}
